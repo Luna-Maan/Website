31b1edee564e997a406cce2e058a05ac56b1610d
Luna Maan <pcvanoostveen@gmail.com>
2024-02-19 02:05:20 +0100

    password manager?

--- chat/chat.js
+++ chat/chat.js
@@ -1,13 +1,53 @@
 console.log("Chat.js");
 let url = "https://wt.ops.labs.vu.nl/api24/9fd5e7bd"
 
-async function formHandler(event) {
-    event.preventDefault();
+let beginID;
+
+async function Hash(string) {
+    const encoder = new TextEncoder();
+    const arrayBuffer = encoder.encode(string);
+
+    const hashAsArrayBuffer = await crypto.subtle.digest("SHA-256", arrayBuffer);
+
+
+    const uint8ViewOfHash = new Uint8Array(hashAsArrayBuffer);
+
+    const hashAsString = Array.from(uint8ViewOfHash)
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+    return hashAsString;
+}
+
+async function loadHandler() {
     let box = document.getElementById("chatBox");
+    let response = await fetch(url);
+    let content = await response.text();
+
+    json = JSON.parse(content);
+
+    console.log(json);
+
+    if (json.length != 0) {
+        beginID = json[0]["id"];
+
+        box.innerHTML = "";
+        for (let i = 0; i < json.length; i++) {
+            message = json[i]["name"] + ": " + json[i]["description"]
+            box.innerHTML += message + "<br>";
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadHandler)
+
+async function chatHandler(event) {
+    if (event.key != "Enter") {
+        return;
+    }
     let object = {};
 
-    let formData = new FormData(form);
-    let message = formData.get("chatInput");
+    let message = this.value;
+    this.value = "";
 
     let name = document.getElementById("name").value;
     object["name"] = name;
@@ -28,39 +68,36 @@ async function formHandler(event) {
         }
     });
 
-    console.log(response);
-
-    response = await fetch(url);
-    let content = await response.text();
-
-    json = JSON.parse(content);
-
-    console.log(json);
-
-    box.innerHTML = "";
-    for (let i = 0; i < json.length; i++) {
-        message = json[i]["name"] + ": " + json[i]["description"]
-        box.innerHTML += message + "<br>";
-    }
-    /*
-    event.preventDefault();
-    let message = document.querySelector("input").value;
-    let response = await fetch("/chat", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message })
-    });
-    let data = await response.json();
-    console.log(data);
-    let chat = document.querySelector("#chat");
-    let newMessage = document.createElement("p");
-    newMessage.textContent = data.message;
-    chat.appendChild(newMessage);
-    document.querySelector("input").value = "";
-    */
+    loadHandler();
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", formHandler);
+let chat = document.getElementById("chatInput");
+chat.addEventListener("keyup", chatHandler);
+
+async function resetHandler(event) {
+    event.preventDefault();
+    let password = document.getElementById("password").value;
+    let hash = await Hash(password);
+    console.log(hash);
+    if (hash == "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8") {
+        await fetch(url + "/reset");
+        console.log(beginID)
+        let promises = [];
+        for (let i = beginID; i < beginID + 9; i++) {
+            promises.push(fetch(url + "/item/" + i, {
+                method: "DELETE"
+            }));
+        }
+        await Promise.all(promises);
+
+        console.log("reset");
+
+        let box = document.getElementById("chatBox");
+        box.innerHTML = "";
+    }
+
+    this.submit();
+}
+
+let reset = document.querySelector("form");
+reset.addEventListener("submit", resetHandler);
